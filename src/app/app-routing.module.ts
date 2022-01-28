5838dd6eb59dbf15ffcb20e0b7108213d6b66e66
Dev Niklesh <devniklesh@gmail.com>
2022-01-28 13:03:29 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CheckoutComponent } from './checkout/checkout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "checkout", pathMatch: "full" },
+  { path: "checkout", component: CheckoutComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
